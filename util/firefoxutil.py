84a1c50ef5a9ffab25066e1b08453f82d415d3d9
Ariesguopei <1693851251@qq.com>
2017-09-27 10:23:32 +0800

    firefox文件做了一下修改

---
+++ util/firefoxutil.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as  EC
+from selenium.webdriver.common.by import By
+
+class startFireFox(object):
+    def __init__(self):
+        pass
+    def startMozillaFirefox(self,url):
+        self.driver=webdriver.Firefox()
+        self.driver.maximize_window()
+        self.driver.get(url)
+        pass
+    #设置休眠时间
+    def TimeSleep(self,number):
+        time.sleep(number)
+        pass
+
